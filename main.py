b29500615c8d701c393c25f69639029acb0af4ba
SergiyShcherbakov <grod13245@gmail.com>
2023-10-16 10:03:06 +0300

    Solution

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 
 
 def quadratic_solver(a, b, c):
-    ...
-
-
-
-
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        root1 = (-b + discriminant**0.5) / (2*a)
+        root2 = (-b - discriminant**0.5) / (2*a)
+        return (root1, root2)
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return  (root,)
+    else:
+        return None
 # Приклад використання функції
 a = 1
 b = -3
